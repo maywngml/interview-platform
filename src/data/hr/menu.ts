441ab37187c2177f48caa63c02998fcb66b3e5a5
서주희 <maywngml@naver.com>
2023-09-04 01:19:09 +0900

    [Update] 메뉴 href 수정

--- src/data/hr/menu.ts
+++ src/data/hr/menu.ts
@@ -1,23 +1,25 @@
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import PersonIcon from '@mui/icons-material/Person';
-import type { menuType } from 'src/types/utils/menu'
+import type { menuType } from 'src/types/utils/menu';
 
 export const menu: menuType[] = [
-    {
-        id: 'home',
-        href: '/',
-        name: '전체 면접 일정',
-        Icon: EventNoteIcon
-    }, {
-        id: 'recruitment',
-        href: '/recruitment',
-        name: '모집 일정 관리',
-        Icon: AssignmentIndIcon
-    }, {
-        id: 'interviewer',
-        href: '/interviewer',
-        name: '면접관 관리',
-        Icon: PersonIcon
-    }
-]
+  {
+    id: 'home',
+    href: '/hr',
+    name: '전체 면접 일정',
+    Icon: EventNoteIcon,
+  },
+  {
+    id: 'recruitment',
+    href: '/hr/recruitment',
+    name: '모집 일정 관리',
+    Icon: AssignmentIndIcon,
+  },
+  {
+    id: 'interviewer',
+    href: '/hr/interviewer',
+    name: '면접관 관리',
+    Icon: PersonIcon,
+  },
+];
