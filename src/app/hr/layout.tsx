ed91b062b0e9a6174b2734a9975a1310852be831
서주희 <maywngml@naver.com>
2023-09-04 01:12:42 +0900

    [Update] hr 공통 레이아웃 children section 태그로 감싸고 section css 조정

--- src/app/hr/layout.tsx
+++ src/app/hr/layout.tsx
@@ -1,15 +1,11 @@
 import { Header, Sidebar } from 'src/components/common';
 
-export default function HRLayout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
-    return (
-        <div>
-            <Header></Header>
-            <Sidebar></Sidebar>
-            {children}
-        </div>
-    )
+export default function HRLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <Header></Header>
+      <Sidebar></Sidebar>
+      <section className='w-[calc(100%-300px)] float-right'>{children}</section>
+    </div>
+  );
 }
