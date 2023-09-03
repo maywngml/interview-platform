64c5043d118a55554455935e267bfc90a95ffd47
서주희 <maywngml@naver.com>
2023-09-04 01:14:31 +0900

    [Add] hr 전체 면접 일정 페이지 - 페이지 제목, 달력 추가

--- src/app/hr/page.tsx
+++ src/app/hr/page.tsx
@@ -1,3 +1,12 @@
+import { WeeklyCalendar } from 'src/components/common';
+
 export default function HR() {
-    return <div></div>
+  return (
+    <section className='p-[40px]'>
+      <h3 className='text-[25px] font-bold mb-[20px] leading-normal'>
+        전체 면접 일정
+      </h3>
+      <WeeklyCalendar />
+    </section>
+  );
 }
