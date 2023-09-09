6be32ab5562bd8e3ad80bbc8f5150ee5cbe1741e
서주희 <maywngml@naver.com>
2023-09-10 02:56:07 +0900

    [FIX] 'use client' 오류 수정

--- src/components/ui/PasswordInput/PasswordInput.tsx
+++ src/components/ui/PasswordInput/PasswordInput.tsx
@@ -1,35 +1,62 @@
-import { useState, InputHTMLAttributes, Dispatch, SetStateAction, ChangeEvent } from 'react';
-import cn from 'clsx'
+'use client';
+import {
+  useState,
+  InputHTMLAttributes,
+  Dispatch,
+  SetStateAction,
+  ChangeEvent,
+} from 'react';
+import cn from 'clsx';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import s from './PasswordInput.module.css'
+import s from './PasswordInput.module.css';
 
 interface PasswordInputProps extends InputHTMLAttributes<HTMLInputElement> {
-    className?: string;
-    value: string;
-    changeValue: Dispatch<SetStateAction<string>>;
+  className?: string;
+  value: string;
+  changeValue: Dispatch<SetStateAction<string>>;
 }
 
 export default function PasswordInput(props: PasswordInputProps) {
-    const { className, value, changeValue, ...rest } = props;
-    const [isFocus, setIsFocus] = useState<boolean>(false);
-    const [isVisible, setIsVisible] = useState<boolean>(false)
+  const { className, value, changeValue, ...rest } = props;
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
 
-    const handleClick = () => {
-        setIsVisible(prevVisible => !prevVisible)
-    }
+  const handleClick = () => {
+    setIsVisible((prevVisible) => !prevVisible);
+  };
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        changeValue(e.target.value);
-    }
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    changeValue(e.target.value);
+  };
 
-    const handleFocusInOut = () => {
-        setIsFocus(prevFocus => !prevFocus)
-    }
+  const handleFocusInOut = () => {
+    setIsFocus((prevFocus) => !prevFocus);
+  };
 
-    return <div className={cn(className, s.passwordWrapper, isFocus && s.focus)} onFocus={handleFocusInOut} onBlur={handleFocusInOut}>
-        <input className={s.password} type={isVisible ? 'text' : 'password'} value={value} onChange={handleChange} {...rest}>
-        </input> {
-            isVisible ? <Visibility className={s.visibilityIcon} onClick={handleClick} /> : <VisibilityOff className={s.visibilityIcon} onClick={handleClick} />
-        }
+  return (
+    <div
+      className={cn(className, s.passwordWrapper, isFocus && s.focus)}
+      onFocus={handleFocusInOut}
+      onBlur={handleFocusInOut}
+    >
+      <input
+        className={s.password}
+        type={isVisible ? 'text' : 'password'}
+        value={value}
+        onChange={handleChange}
+        {...rest}
+      ></input>{' '}
+      {isVisible ? (
+        <Visibility
+          className={s.visibilityIcon}
+          onClick={handleClick}
+        />
+      ) : (
+        <VisibilityOff
+          className={s.visibilityIcon}
+          onClick={handleClick}
+        />
+      )}
     </div>
+  );
 }
